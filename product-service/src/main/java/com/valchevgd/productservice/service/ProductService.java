1c669abf1bfc99d287afeeeaec078999b6986c54
Georgi Valchev <valchevgd@gmail.com>
2022-12-16 21:53:49 +0200

    Added Product Service

---
+++ product-service/src/main/java/com/valchevgd/productservice/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.valchevgd.productservice.service;
+
+import com.valchevgd.productservice.dto.ProductRequest;
+
+public interface ProductService {
+
+    void createProduct(ProductRequest productRequest);
+}
---
+++ product-service/src/main/java/com/valchevgd/productservice/service/ProductServiceImpl.java
@@ -0,0 +1,28 @@
+package com.valchevgd.productservice.service;
+
+import com.valchevgd.productservice.dto.ProductRequest;
+import com.valchevgd.productservice.model.Product;
+import com.valchevgd.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductServiceImpl implements ProductService{
+
+    private final ProductRepository productRepository;
+    @Override
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+
+        log.info("Product with id {} is created", product.getId());
+    }
+}
