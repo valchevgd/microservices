e5328ea05e1273bed977fd1e3599115fea902906
Georgi Valchev <valchevgd@gmail.com>
2022-12-16 21:57:42 +0200

    Added createProduct method in ProductController

--- product-service/src/main/java/com/valchevgd/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/valchevgd/productservice/controller/ProductController.java
@@ -1,9 +1,20 @@
 package com.valchevgd.productservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.valchevgd.productservice.dto.ProductRequest;
+import com.valchevgd.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
 }
