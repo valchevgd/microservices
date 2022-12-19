05c991ff8a77d4842864f0bb7293e4ecd242873a
Georgi Valchev <valchevgd@gmail.com>
2022-12-19 23:30:50 +0200

    Added Order Service models

---
+++ order-service/src/main/java/com/valchevgd/orderservice/model/OrderLineItem.java
@@ -0,0 +1,22 @@
+package com.valchevgd.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_line_items")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
